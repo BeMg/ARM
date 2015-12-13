313da1609e5d26ca34bb24e3b2daa98a682ef160
BeMg <gccbg04538@gmail.com>
2015-12-13 11:19:20 +0800

    remember ldr and str

--- hw7/NumSort.s
+++ hw7/NumSort.s
@@ -13,24 +13,26 @@
 NumSort:
     /* function start */
     MOV ip, sp
-    STMFD sp!, {r0-r10, fp, ip, lr, pc}
+    STMFD sp!, {r4-r10, fp, ip, lr, pc}
     SUB fp, ip, #4
 
     MOV r5, r0 /* get array size */
     MOV r6, r1 /* get array address */
 
-    MOV r8, #100
     LDR r6, .result
     MOV r7, r6
+    MOV r8, #100
 LOOP:
-    LDR r6, [r1], #4
-    MOV r9, r6
-    LDR r6, [r9], #4
-    MOV r6, r9
+    LDR r9, [r1], #4
+    STR r9, [r6], #4
     SUB r8, r8, #1
     CMP r8, #0
     BNE LOOP
     MOV r6, r7
+
+
+
+
 
 /*
 for(int i=0; i<n; i++){
@@ -40,10 +42,38 @@ for(int i=0; i<n; i++){
     }
 }
 */
+    MOV r7, #0
+    MOV r8, #0
+    MOV r7, r5
+
+LOOPi:
+    MOV r4, r6
+    SUB r7, r7, #1
+    MOV r8, r7
+LOOPj:
+    CMP r8, #0
+    BEQ FIN
+    SUB r8, r8, #1
+    LDR r9, [r4]
+    LDR r10, [r4, #4]!
+    CMP r10, r9
+    BMI SWAP
+    B END
+SWAP:
+    MOV r1, r9
+    MOV r9, r10
+    MOV r10, r1
+END:
+    STR r9, [r4, #-4]
+    STR r10, [r4]
+    CMP r8, #0
+    BNE LOOPj
+    CMP r7, #0
+    BNE LOOPi
 
 FIN:
     MOV r0, r6
     /* function exit */
-    LDMEA fp, {r0-r10, fp, sp, pc}
+    LDMEA fp, {r4-r10, fp, sp, pc}
     nop
     .end
